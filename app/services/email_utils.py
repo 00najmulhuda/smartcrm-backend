1704a31c87693ae93514f1b79f768d08239c3c68
Najmul Huda <nh5037480@gmail.com>
2026-07-11 16:14:41 +0530

    debug brevo smtp

--- app/services/email_utils.py
+++ app/services/email_utils.py
@@ -1,6 +1,6 @@
-import os 
-import smtplib 
-from email.message import EmailMessage 
+import os
+import smtplib
+from email.message import EmailMessage
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -8,20 +8,41 @@ load_dotenv()
 SMTP_EMAIL = os.getenv("SMTP_EMAIL")
 SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
 SMTP_SERVER = os.getenv("SMTP_SERVER")
-SMTP_PORT = os.getenv("SMTP_PORT")
+SMTP_PORT = int(os.getenv("SMTP_PORT"))
 
-def send_email(to_email:str, subject:str,body:str):
+
+def send_email(to_email: str, subject: str, body: str):
+
+    print("=" * 50)
+    print("SMTP_SERVER :", SMTP_SERVER)
+    print("SMTP_PORT   :", SMTP_PORT)
+    print("SMTP_EMAIL  :", SMTP_EMAIL)
+    print("=" * 50)
+
     message = EmailMessage()
 
     message["From"] = SMTP_EMAIL
     message["To"] = to_email
     message["Subject"] = subject
-
     message.set_content(body)
 
-    with smtplib.SMTP(SMTP_SERVER,int(SMTP_PORT)) as server:
+    with smtplib.SMTP(
+        SMTP_SERVER,
+        SMTP_PORT,
+        timeout=20
+    ) as server:
+
+        server.set_debuglevel(1)
+
+        print("Connected to SMTP Server")
+
         server.starttls()
-        server.login(SMTP_EMAIL,SMTP_PASSWORD)
+        print("TLS Started")
+
+        server.login(SMTP_EMAIL, SMTP_PASSWORD)
+        print("Logged In")
+
         server.send_message(message)
+        print("Email Sent Successfully")
 
 
